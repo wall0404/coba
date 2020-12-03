d7c1e2740f7e3b4e0005e4b39c6b53909f0e5811
Marc Wall <github@marc-wall.de>
2020-12-03 20:25:44 +0100

    implements day picker

--- public/js/app.js
+++ public/js/app.js
@@ -86,6 +86,41 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/Elements/DayPicker.vue?vue&type=script&lang=js&":
+/*!*****************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/Elements/DayPicker.vue?vue&type=script&lang=js& ***!
+  \*****************************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+//
+//
+//
+//
+//
+//
+/* harmony default export */ __webpack_exports__["default"] = ({
+  name: "DayPicker",
+  //props: ['days'],
+  data: function data() {
+    return {
+      days: [{
+        day: "Montag",
+        color: "green",
+        selected: false
+      }, {
+        day: "Dienstag",
+        color: "orange",
+        selected: true
+      }]
+    };
+  }
+});
+
+/***/ }),
+
 /***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/Elements/Modal.vue?vue&type=script&lang=js&":
 /*!*************************************************************************************************************************************************************************!*\
   !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/Elements/Modal.vue?vue&type=script&lang=js& ***!
@@ -466,6 +501,10 @@ __webpack_require__.r(__webpack_exports__);
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _Global_Spinner__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../Global/Spinner */ "./resources/js/components/Global/Spinner.vue");
+/* harmony import */ var _Elements_DayPicker__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../Elements/DayPicker */ "./resources/js/components/Elements/DayPicker.vue");
+//
+//
+//
 //
 //
 //
@@ -481,9 +520,11 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 
+
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "Page_DateTimeSelection",
   components: {
+    DayPicker: _Elements_DayPicker__WEBPACK_IMPORTED_MODULE_1__["default"],
     Spinner: _Global_Spinner__WEBPACK_IMPORTED_MODULE_0__["default"]
   },
   data: function data() {
@@ -2090,6 +2131,47 @@ exports.clearImmediate = (typeof self !== "undefined" && self.clearImmediate) ||
 
 /***/ }),
 
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/Elements/DayPicker.vue?vue&type=template&id=1febfe86&scoped=true&":
+/*!*********************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/Elements/DayPicker.vue?vue&type=template&id=1febfe86&scoped=true& ***!
+  \*********************************************************************************************************************************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c(
+    "div",
+    { staticClass: "coba-utilization-indicator-container" },
+    _vm._l(_vm.days, function(day) {
+      return _c(
+        "div",
+        {
+          class:
+            "coba-utilization-indicator coba-utilization-indicator-" +
+            day.color +
+            " " +
+            (day.selected ? "coba-utilization-indicator-selected" : "")
+        },
+        [_vm._v(_vm._s(day.day.substring(0, 1)))]
+      )
+    }),
+    0
+  )
+}
+var staticRenderFns = []
+render._withStripped = true
+
+
+
+/***/ }),
+
 /***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/Elements/Modal.vue?vue&type=template&id=71c737c0&scoped=true&":
 /*!*****************************************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/Elements/Modal.vue?vue&type=template&id=71c737c0&scoped=true& ***!
@@ -2586,6 +2668,8 @@ var render = function() {
         _vm._v(_vm._s(_vm.$route.params.workstation_id))
       ])
     ]),
+    _vm._v(" "),
+    _c("div", { staticClass: "coba-container" }, [_c("DayPicker")], 1),
     _vm._v(" "),
     _vm._m(1)
   ])
@@ -19823,6 +19907,75 @@ var app = new Vue({
   },
   el: '#app'
 });
+
+/***/ }),
+
+/***/ "./resources/js/components/Elements/DayPicker.vue":
+/*!********************************************************!*\
+  !*** ./resources/js/components/Elements/DayPicker.vue ***!
+  \********************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _DayPicker_vue_vue_type_template_id_1febfe86_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./DayPicker.vue?vue&type=template&id=1febfe86&scoped=true& */ "./resources/js/components/Elements/DayPicker.vue?vue&type=template&id=1febfe86&scoped=true&");
+/* harmony import */ var _DayPicker_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./DayPicker.vue?vue&type=script&lang=js& */ "./resources/js/components/Elements/DayPicker.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
+
+
+
+
+/* normalize component */
+
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _DayPicker_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
+  _DayPicker_vue_vue_type_template_id_1febfe86_scoped_true___WEBPACK_IMPORTED_MODULE_0__["render"],
+  _DayPicker_vue_vue_type_template_id_1febfe86_scoped_true___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+  false,
+  null,
+  "1febfe86",
+  null
+  
+)
+
+/* hot reload */
+if (false) { var api; }
+component.options.__file = "resources/js/components/Elements/DayPicker.vue"
+/* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/components/Elements/DayPicker.vue?vue&type=script&lang=js&":
+/*!*********************************************************************************!*\
+  !*** ./resources/js/components/Elements/DayPicker.vue?vue&type=script&lang=js& ***!
+  \*********************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_DayPicker_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/babel-loader/lib??ref--4-0!../../../../node_modules/vue-loader/lib??vue-loader-options!./DayPicker.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/Elements/DayPicker.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_DayPicker_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./resources/js/components/Elements/DayPicker.vue?vue&type=template&id=1febfe86&scoped=true&":
+/*!***************************************************************************************************!*\
+  !*** ./resources/js/components/Elements/DayPicker.vue?vue&type=template&id=1febfe86&scoped=true& ***!
+  \***************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_DayPicker_vue_vue_type_template_id_1febfe86_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib??vue-loader-options!./DayPicker.vue?vue&type=template&id=1febfe86&scoped=true& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/Elements/DayPicker.vue?vue&type=template&id=1febfe86&scoped=true&");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_DayPicker_vue_vue_type_template_id_1febfe86_scoped_true___WEBPACK_IMPORTED_MODULE_0__["render"]; });
+
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_DayPicker_vue_vue_type_template_id_1febfe86_scoped_true___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
+
+
 
 /***/ }),
 
---
+++ resources/js/components/Elements/DayPicker.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="coba-utilization-indicator-container">
+        <div v-for="day in days" :class="'coba-utilization-indicator coba-utilization-indicator-'+day.color+' '+(day.selected?'coba-utilization-indicator-selected':'')" >{{day.day.substring(0,1)}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DayPicker",
+    //props: ['days'],
+    data() {
+        return {
+            days: [
+                {day: "Montag", color:"green", selected: false},
+                {day: "Dienstag", color:"orange", selected: true}
+            ]
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
