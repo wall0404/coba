4406209d3155eb0142f879e9f9e6311e334e75da
Yaxi999 <75373563+Yaxi999@users.noreply.github.com>
2020-12-17 21:22:39 +0100

    Create Profile_Edit.vue

---
+++ resources/js/components/Pages/Profile_Edit.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="coba-page">
+        <div class="coba-container cobe-flex coba-header">
+            <span class="coba-page-headline">Personenprofile</span>
+        </div>
+
+        <div class="coba-container">
+            <span class="coba-page-text">Vorname: {{$store.getters.data.user.firstName}}</span>
+            <input type="button" value="bearbeiten" style="float: right">
+        </div>
+
+        <div class="coba-container">
+            <span class="coba-page-text">Nachname: {{$store.getters.data.user.lastName}}</span>
+            <input type="button" value="bearbeiten" style="float: right">
+        </div>
+
+        <div class="coba-container">
+            <span class="coba-page-text">Email: {{$store.getters.data.user.email}}</span>
+            <input type="button" value="bearbeiten" style="float: right">
+        </div>
+
+        <div class="coba-container">
+            <span class="coba-page-text">Passwort: </span>
+            <input type="button" value="bearbeiten" style="float: right">
+        </div>
+        <div class="coba-container">
+            <span class="coba-page-text">Lieblingssitzplätze: </span>
+            <input type="button" value="bearbeiten" style="float: right">
+        </div>
+
+        <div class="coba-container">
+            <span class="coba-page-text">Lieblingsbuddy: </span>
+            <input type="button" value="bearbeiten" style="float: right">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Profile_Edit"
+}
+</script>
+
+<style scoped>
+
+</style>
