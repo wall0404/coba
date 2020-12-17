d5b5a3697a76a8170d84a79c64ceddc44c93faeb
yuyuRui <36037786+yuyuRui@users.noreply.github.com>
2020-12-18 00:08:59 +0800

    Create Page_Favoriteseat.vue

---
+++ resources/js/components/Pages/Page_Favoriteseat.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="coba-page">
+        <div class="coba-container coba-flex coba-header">
+            <span class="coba-page-headline">FavoriteSeat</span>
+        </div>
+
+        <div class="coba-container">
+            <span class="coba-text-big">Favorite Seat:</span>
+            <select name="select" id="selection">
+                <option value="First Choice">Platz 1</option>
+                <option value="Second Choice">Platz 2</option>
+                <option value="Third Choice">Platz 3</option>
+                <option value="Fourth Choice">Platz 4</option>
+                <option value="Fifth Choice">Platz 5</option>
+                <option value="Sixth Choice">Platz 6</option>
+                <option value="Seventh Choice">Platz 7</option>
+                <option value="Eighth Choice">Platz 8</option>
+                <option value="Ninth Choice">Platz 9</option>
+                <option value="Tenth Choice">Platz 10</option>
+            </select>
+        </div>
+
+
+    </div>
+
+
+</template>
+
+<script>
+export default {
+name: "Teampage"
+}
+
+</script>
+
+<style scoped>
+
+
+</style>
+
