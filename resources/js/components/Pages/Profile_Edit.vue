94c641368472ae641d65bce57924e1697fbc8ea8
Yaxi999 <75373563+Yaxi999@users.noreply.github.com>
2020-12-23 19:06:39 +0100

    Create Profile_Edit.vue

---
+++ resources/js/components/Pages/Profile_Edit.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="coba-page">
+        <div class="coba-container cobe-flex coba-header">
+            <span class="coba-page-headline">Personenprofile</span>
+        </div>
+
+        <div class="coba-container">
+            <span class="coba-page-text">Vorname: {{$store.getters.data.user.firstName}}</span>
+            <div class="edit-button">
+                <button class="small button">bearbeiten
+                </button>
+            </div>
+        </div>
+
+        <div class="coba-container">
+            <span class="coba-page-text">Nachname: {{$store.getters.data.user.lastName}}</span>
+            <div class="edit-button">
+                <button class="small button">bearbeiten
+                </button>
+            </div>
+        </div>
+
+
+        <div class="coba-container">
+            <span class="coba-page-text">Email: {{$store.getters.data.user.email}}</span>
+            <div class="edit-button">
+                <button class="small button">bearbeiten
+                </button>
+            </div>
+        </div>
+
+        <div class="coba-container">
+            <span class="coba-page-text">Passwort: </span>
+            <div class="edit-button">
+                <button class="small button">bearbeiten
+                </button>
+            </div>
+        </div>
+        <div class="coba-container">
+            <span class="coba-page-text">Lieblingssitzplätze:</span>
+            <div class="edit-button">
+                <button class="small button">bearbeiten
+                </button>
+            </div>
+        </div>
+
+        <div class="coba-container">
+            <span class="coba-page-text">Lieblingsbuddy:</span>
+            <div class="edit-button">
+                <button class="small button">bearbeiten
+                </button>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "Profile_Edit"
+}
+</script>
+
+<style scoped>
+.edit-button {
+    float: right;
+}
+</style>
