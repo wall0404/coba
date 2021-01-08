2197118540f8f40ac4b8e46604569b8cafc6fc62
Marc Wall <github@marc-wall.de>
2021-01-08 16:17:40 +0100

    adds back button

---
+++ resources/js/components/Global/BackButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="back_button">
+        <b-icon icon="arrow-left-short" font-scale="3"></b-icon>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BackButton",
+    data() {
+        return {
+            exclude: [
+                ""
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+    #back_button {
+        position: fixed;
+        top: 20px;
+        left: 20px;
+    }
+</style>
