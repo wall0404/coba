0fb591a032f5ea4d14c04572a010a9cc03e3ce4b
Marc Wall <github@marc-wall.de>
2021-01-22 10:14:07 +0100

    add route for backend

--- database/factories/BookingFactory.php
+++ database/factories/BookingFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\Booking;
 use App\Models\User;
 use App\Models\Workstation;
+use Carbon\Carbon;
 use DateTime;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Date;
@@ -27,21 +28,23 @@ class BookingFactory extends Factory
      */
     public function definition()
     {
-        $date = new DateTime();
-        $date->modify('-7 days');
+        $date = new Carbon();
+        $date->addDays(-7);
         $user_id = 1;
         $workstation_id = 1;
 
-        $found = true;
-        while($found) {
+        $error = true;
+        while($error) {
             try {
-                $date->modify('+' . random_int(0, 21) . ' days');
+                $date->addDays(random_int(0, 21));
                 $user_id = random_int(1,(User::all()->count()));
                 $workstation_id = random_int(1,Workstation::all()->count());
             } catch (\Exception $e) {
 
             }
-            $found = Booking::where('user_id', $user_id)->where('date', $date)->count() !== 0;
+            $dayOfWeek = $date->dayOfWeek;
+
+            $error = Booking::where('user_id', $user_id)->where('date', $date)->count() !== 0 || $dayOfWeek == 0 || $dayOfWeek == 6 ;
         }
 
         return [
