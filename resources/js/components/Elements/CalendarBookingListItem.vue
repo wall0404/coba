9f84daa8f81d58619baa3c39e74d169a4773b544
mira853 <mira@koellermeier.com>
2021-01-29 09:56:40 +0100

    Pen im Kalender
    der Pen muss aber allgemein noch mal überarbeitet werden

--- resources/js/components/Elements/CalendarBookingListItem.vue
+++ resources/js/components/Elements/CalendarBookingListItem.vue
@@ -15,17 +15,48 @@
                      :to="{ name: 'DateTimeSelection', params: { workstation_id:workstation.id, preSelectedDays: [dayObj], calenderBool: true }}">
             <b-icon icon="plus"></b-icon>
         </router-link>
-        <!-- Anzeige des Buchungsbutton wenn Buchung vom angemeldeten User ist -->
-        <router-link class="table-item icon" v-else
-                     v-bind:to="'/booking/'+booking.id">
-            <b-icon icon="pencil"></b-icon>
-        </router-link>
+        <!-- Drop Down list with pencil icon to toggle it -->
+        <div class="table-item icon coba-dropdown-container" @click="toggleDropDown(booking)" v-else>    <!-- @click="openDropDown(booking)" - Triggerbox around the pencil icon, it opens a drop down List-->
+            <!-- Pencil Icon inside the trigger box -> will have a white background when drop down opens-->
+            <div :class="{'grey-background':dropDown.open&&dropDown.id === booking.id}">
+                <b-icon icon="pencil"></b-icon>
+            </div>
+            <!-- Drop Down start -->
+            <div v-if="dropDown.open && dropDown.id === booking.id" class="coba-dropdown-wrapper" style="bottom: -52px; z-index: 2; right: -11px">
+                <div class="coba-dropdown-content" style="background-color: #EBEBEB">
+                    <ul class="coba-list-nobullets" style="margin: 0px">
+                        <li style="border-bottom: 1px solid #505050"> <button style="background-color:rgba(255,255,255,0);">Bearbeiten</button> </li>
+                        <li class="last" > <button style="background-color:rgba(255,255,255,0);" @click="openModal(booking)">Löschen</button> </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- drop down end -->
+        </div>
+        <!-- modalDel -> A modal which asks if you really want to delete a booking -->
+        <modal :show-modal="modalDel.open" @modal-close-event="closeModal" @modal-positive-event="deleteBooking">
+            <template v-slot:header>
+                <div class="coba-modal-header">Buchung entfernen</div>
+            </template>
+            <template v-slot:body>
+                <div class="coba-modal-body">
+                    Bist du dir sicher, dass du die Buchung entfernen möchtest?
+                </div>
+            </template>
+            <template v-slot:footer>
+                <div class="coba-modal-footer coba-button-container">
+                    <button class="coba-button coba-button-danger" @click="deleteBooking(modalDel.header)">Ja</button>
+                    <button class="coba-button" @click="closeModal">Nein</button>
+                </div>
+            </template>
+        </modal>
     </div>
 </template>
 
 <script>
+import Modal from "./Modal";
 export default {
     name: "CalendarBookingListItem",
+    components: {Modal},
     props: ['workstation', 'bookings', 'users', 'date'],
     data() {
         return {
@@ -37,7 +68,15 @@ export default {
             today: new Date(),
             dayObj: {
                 date: new Date(this.date)
-            }
+            },
+            dropDown: {
+                id: "",
+                open: false
+            },
+            modalDel: {
+                header: "",
+                open: false,
+            },
         }
     },
     created() {
@@ -47,8 +86,6 @@ export default {
                 bookings_for_this_workstation.push(this.bookings[i]);
             }
         }
-
-
         this.calcInfo(bookings_for_this_workstation)
     },
     methods: {
@@ -88,6 +125,48 @@ export default {
             this.name = user.firstName + " " + user.lastName;
             this.time = (startBooking.from.substr(0,5) +" - " + endBooking.to.substr(0,5));
         },
+        toggleDropDown(booking){
+            //this.dropDown.open = true;
+            if (this.dropDown.open==true){
+                this.dropDown.open = false;
+            }
+            else this.dropDown.open = true;
+            this.dropDown.id = booking.id;
+        },
+        //all methods for the delete-modal
+        openModal(booking) {
+            this.modalDel.header = booking.id;
+            this.modalDel.open = true;
+        },
+        closeModal() {
+            this.modalDel.open = false;
+        },
+        deleteBooking(id) {
+            this.showModal = false;
+            //this.load = true;
+            fetch('/api/booking/'+id, {
+                method: 'DELETE',
+                headers: {
+                    'content-type': 'application/json',
+                    'Authorization' : 'Bearer '+localStorage.token
+                }
+            })
+                .then(res => res.json())
+                .then(res => {
+                    if(res.success) {
+                        //this.load = false;
+                        this.$router.go();
+                    }
+                    else {
+                        this.error = true;
+                        //this.load = false;
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                    //this.load = false;
+                })
+        },
     }
 }
 </script>
@@ -95,6 +174,7 @@ export default {
 <style scoped>
 .booking {
     display: flex;
+    position: relative;
 }
 .booking.strong {
     font-weight: bold;
@@ -118,5 +198,14 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+}
+.grey-background{
+    background-color: #EBEBEB;
+    border-top-right-radius: 10px;
+    border-top-left-radius: 10px;
+    position: absolute;
+    padding: 10px;
+    padding-top: 7px;
+    z-index: 1;
 }
 </style>
