5e38f31b2c8b6becb67d84fb275dd457a549c523
thelinking <75447731+thelinking@users.noreply.github.com>
2021-02-03 23:45:06 +0100

    Umbuchungseite erstellt

---
+++ resources/js/components/Pages/Book/Page_ChangeBooking.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="coba-page">
+        <div class="coba-container coba-flex coba-header">
+            <span class="coba-page-headline" align="center">Was möchtes du ändern?</span>
+        </div>
+
+        <!-- Tag der Buchung -->
+        <div class="coba-container">
+            Tag
+        </div>
+        <!-- Zeit der Buchung -->
+        <div class="coba-container">
+            Zeit
+        </div>
+        <!-- Sitzplatz -->
+        <div class="coba-container">
+            Sitz
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Page_ChangeBooking",
+}
+</script>
+
+<style scoped>
+
+</style>
