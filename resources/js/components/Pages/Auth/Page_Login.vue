09f38eb272d006dde318776ed2146b10861df7ef
Hannah <hmg.h@web.de>
2021-02-18 10:28:30 +0100

    Änderung Schrift Header

--- resources/js/components/Pages/Auth/Page_Login.vue
+++ resources/js/components/Pages/Auth/Page_Login.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="coba-container coba-flex coba-header mb-4" style="flex-direction: column">
             <img src="/img/Coba_Logo.png" style="text-align: center; margin-bottom: 20px" height="70px">
-            <span class="coba-page-headline">Willkommen zurück</span>
+            <span class="coba-page-headline">Willkommen</span>
         </div>
         <div class="coba-container">
             <form class="coba-form">
